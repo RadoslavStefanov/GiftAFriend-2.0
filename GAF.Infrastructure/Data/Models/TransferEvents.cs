ada481739bf03a0bf6100d5b915f26c279873592
RadoslavStefanov <radoslav.g.stefanov@gmail.com>
2022-05-14 14:31:03 +0300

    Started the DB creation

---
+++ GAF.Infrastructure/Data/Models/TransferEvents.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAF.Infrastructure.Data.Models
+{
+    public class TransferEvents
+    {
+        public int Id { get; set; }
+        public string EventType { get; set; }
+        public DateTime DateTime { get; set; }
+        public int CallerId { get; set; }
+        public int CallerName { get; set; }
+        public int RecieverId { get; set; }
+        public int RecieverName { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ GAF.Infrastructure/Data/Models/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿
+namespace GAF.Infrastructure.Data.Models
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int MobileNumber { get; set; }
+        public decimal GiftTokens { get; set; }
+        public decimal Money { get; set; }
+        public string Address { get; set; }
+    }
+}
