2a30536812a99be8a8d93b065b1758da2abb1481
RadoslavStefanov <radoslav.g.stefanov@gmail.com>
2022-05-16 07:29:58 +0300

    Reworked the notification concept and fixed a UI big

--- GiftAFriend 2.0/wwwroot/js/notificationHandler.js
+++ GiftAFriend 2.0/wwwroot/js/notificationHandler.js
@@ -2,11 +2,17 @@
 {
     let notificationNodesArray = input.split('-');
 
+    let notificationNode = document.createElement('span');
+    notificationNode.classList.add("badge");
+    notificationNode.classList.add("badge-info");
+    notificationNode.classList.add("right");
+    notificationNode.innerText = "!";
+
     for (let i = 0; i < notificationNodesArray.length; i++)
     {
         let node = document.getElementById(notificationNodesArray[i])
         if (node != undefined)
-        { node.style.display="block" }
+        { node.appendChild(notificationNode) }
     }
 
 }
